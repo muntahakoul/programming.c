fbb06e8dcdf439170605f4c9713b9ae89c4a2356
muntahakoul <muntahakoul1906@gmail.com>
2024-07-21 11:09:37 +0100

    Add files via upload

---
+++ display 1000-100.cpp
@@ -0,0 +1,11 @@
+//program to print number from 100 TO 1
+#include <stdio.h>
+
+int main() {
+    int i = 1000;
+    while (i >= 100) {
+        printf("%d ", i);
+        i--;
+    }
+    return 0;
+}
