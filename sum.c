156415be0dda91605c172dac9fe33971f45813fe
muntahakoul <muntahakoul1906@gmail.com>
2024-07-21 10:48:36 +0100

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+//program c to add sum of natural number
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
+
