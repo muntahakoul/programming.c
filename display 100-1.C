e9a7b0569ca5055c44da2bd03ab1e8756b3d7765
muntahakoul <muntahakoul1906@gmail.com>
2024-07-21 11:08:03 +0100

    Add files via upload

---
+++ display 100-1.C
@@ -0,0 +1,11 @@
+//program to print number from 100 TO 1
+#include <stdio.h>
+
+int main() {
+    int i = 100;
+    while (i >= 1) {
+        printf("%d ", i);
+        i--;
+    }
+    return 0;
+}
